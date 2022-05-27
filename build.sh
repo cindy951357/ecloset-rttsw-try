fa1126c64cef8812c9ec4a6a487e18aa06af5714
cindy951357 <cindy951357@gmail.com>
2022-05-27 12:58:44 +0800

    Updates

--- build.sh
+++
@@ -1,2 +0,0 @@
-cp src/index.html dist/index.html
-cp src/index.css dist/index.css
--- deploy.js
+++
@@ -1,6 +0,0 @@
-let ghpages = require('gh-pages');
-
-ghpages.publish('dist', {
-    branch: 'master',
-    repo: 'git@github.com:cindy951357/learn-tailwindcss.git'
-}, function (err) { });
