4b7af9ef1cf659a8c2019bfa89fc8a50431cc241
cindy951357 <cindy951357@gmail.com>
2022-05-27 12:58:18 +0800

    alter repo

--- deploy.js
+++ deploy.js
@@ -2,5 +2,5 @@ let ghpages = require('gh-pages');
 
 ghpages.publish('dist', {
     branch: 'master',
-    repo: 'https://github.com/cindy951357/learn-tailwindcss.git'
+    repo: 'git@github.com:cindy951357/learn-tailwindcss.git'
 }, function (err) { });
