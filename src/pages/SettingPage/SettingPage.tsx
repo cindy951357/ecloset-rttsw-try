1da23401a547f381b5fee5c7d0231e8e352b6ecb
cindy_toshiba <cindy951357@gmail.com>
2022-09-03 12:06:43 +0800

    utilize DopdownInput to switch locale

--- src/pages/SettingPage/SettingPage.tsx
+++ src/pages/SettingPage/SettingPage.tsx
@@ -1,29 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import i18n from 'i18next';
 import classnames from 'classnames';
+import { DropdownInput } from '../../components/DropdownInput/DropdownInput';
 
 const settingPageClass = classnames(
     'page',
     'setting-page',
 
 )
-
+const enName = 'English';
+const twName = '繁體中文';
 const SettingPage = () => {
-    const onLangButtonClick = lang => {
-        i18n.changeLanguage(lang);
-    }
+    const [userLang, setUserLang] = useState('tw');
+
+    useEffect(() => {
+        if (userLang === twName) {
+            i18n.changeLanguage('tw');
+        } else if (userLang === enName) {
+            i18n.changeLanguage('en');
+        }
+    }, [userLang]);
+
     return (
         <div className={settingPageClass}>
-            <button
-                onClick={() => { onLangButtonClick('tw') }}
-            >
-                繁體中文
-            </button>
-            <button
-                onClick={() => { onLangButtonClick('en') }}
-            >
-                English
-            </button>
+            <DropdownInput
+                layer={'FIRST'}
+                setParentSelected={setUserLang}
+                optionList={[enName, twName]}
+                clearSecondLayerDefaultOption={() => { }}
+                secondLayerDefaultOption={''}
+            />
         </div>
     )
 }
